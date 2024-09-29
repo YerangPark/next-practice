3828bed9ae78c4e6ee4b196b04e46753bb96b254
YerangPark <smsms_@naver.com>
2024-09-29 10:02:00 +0900

    feat: 토큰 인증 처리 추가

--- src/app/client-layout.tsx
+++ src/app/client-layout.tsx
@@ -1,14 +1,39 @@
-"use client";
+'use client'
 
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react'
 import { useEffect, useRef } from 'react'
 import { Provider, useDispatch } from 'react-redux'
 import { fetchSkills } from '@/features/skill/skillSlice'
 import store, { AppDispatch } from '@/store'
-import theme from '../theme';
+import isTokenExpired from '@/utils/TokenExpiredChecker'
+import { useRouter } from 'next/navigation'
+import theme from '../theme'
+
+function ClientComponentWrapper({ children }: { children: React.ReactNode }) {
+  const dispatch: AppDispatch = useDispatch()
+  const didFetchRef = useRef(false) // NOTE: 재랜더링해도 값이 저장되는 useRef를 이용해서 최초 1번 실행
+
+  useEffect(() => {
+    if (!didFetchRef.current) {
+      dispatch(fetchSkills())
+      didFetchRef.current = true
+    }
+  }, [dispatch])
+
+  return children
+}
 
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
-  // SECTION: 스킬 정보는 애플리케이션 접근 시 최초 1회만 불러오면 됨.
+  const router = useRouter()
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (!token || isTokenExpired(token)) {
+      localStorage.removeItem('token')
+      router.push('/')
+    }
+  }, [router])
+
   return (
     <Provider store={store}>
       <ClientComponentWrapper>
@@ -16,18 +41,4 @@ export default function ClientLayout({ children }: { children: React.ReactNode }
       </ClientComponentWrapper>
     </Provider>
   )
-}
-
-function ClientComponentWrapper({ children }: { children: React.ReactNode }) {
-  const dispatch: AppDispatch = useDispatch();
-  const didFetchRef = useRef(false);  // NOTE: 재랜더링해도 값이 저장되는 useRef를 이용해서 최초 1번 실행
-
-  useEffect(() => {
-    if (!didFetchRef.current) {
-      dispatch(fetchSkills());
-      didFetchRef.current = true;
-    }
-  }, [dispatch]);
-
-  return <>{children}</>;
 }
