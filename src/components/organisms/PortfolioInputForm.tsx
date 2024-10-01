afc8bea8ca2502cadd12833685afd8decd0f155a
YerangPark <smsms_@naver.com>
2024-10-01 10:06:00 +0900

    feat: 포트폴리오 수정 페이지 구현

--- src/components/organisms/PortfolioInputForm.tsx
+++ src/components/organisms/PortfolioInputForm.tsx
@@ -1,33 +1,73 @@
 import React, { useState } from 'react'
-import { Image, Box, Button, FormControl, FormLabel, Input, Tag, Textarea, Text, Flex, Divider, Wrap, WrapItem, TagLabel, TagCloseButton, HStack, InputGroup, InputRightElement, Icon, List, ListItem } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Tag,
+  Text,
+  Divider,
+  Wrap,
+  WrapItem,
+  TagLabel,
+  TagCloseButton,
+  HStack,
+  InputGroup,
+  InputRightElement,
+  Icon,
+  List,
+  ListItem,
+} from '@chakra-ui/react'
 import { useSelector } from 'react-redux'
 import { RootState } from '@/store'
-import { FiPlus, FiCheck } from "react-icons/fi"
+import { FiPlus, FiCheck } from 'react-icons/fi'
 import { FaSearch } from 'react-icons/fa'
+import { Skill } from '@/types/data'
 import InformationBox from '../molecules/InformationBox'
 import InputTextbox from '../molecules/InputTextbox'
 import InputImage from '../molecules/InputImage'
-
-interface Skill {
-  id: number
-  name: string
-}
 
 const popularSkills: Skill[] = [
   { id: 1, name: 'Javascript' },
   { id: 12, name: 'React' },
   { id: 16, name: 'Next.js' },
-  { id: 11, name: 'Spring' }
+  { id: 11, name: 'Spring' },
 ]
 
-const PortfolioInputForm: React.FC = () => {
-  const [portfolioName, setPortfolioName] = useState('')
-  const [title, setTitle] = useState('')
-  const [description, setDescription] = useState('')
-  const [image, setImage] = useState('')
-  const [githubLink, setGithubLink] = useState('')
-  const [blogLink, setBlogLink] = useState('')
-  const [selectedTechStack, setSelectedTechStack] = useState<number[]>([])
+interface PortfolioInputFormProps {
+  portfolioName: string
+  setPortfolioName: (name: string) => void
+  title: string
+  setTitle: (title: string) => void
+  description: string
+  setDescription: (description: string) => void
+  image: File | string | null
+  setImage: (image: File | string | null) => void
+  githubLink: string
+  setGithubLink: (link: string) => void
+  blogLink: string
+  setBlogLink: (link: string) => void
+  selectedTechStack: number[]
+  setSelectedTechStack: (stack: number[]) => void
+}
+
+const PortfolioInputForm: React.FC<PortfolioInputFormProps> = ({
+  portfolioName,
+  setPortfolioName,
+  title,
+  setTitle,
+  description,
+  setDescription,
+  image,
+  setImage,
+  githubLink,
+  setGithubLink,
+  blogLink,
+  setBlogLink,
+  selectedTechStack,
+  setSelectedTechStack,
+}) => {
   const skills = useSelector((state: RootState) => state.skill.skills)
   const [searchQuery, setSearchQuery] = useState('')
   const [searchResults, setSearchResults] = useState<Skill[]>([])
@@ -38,9 +78,7 @@ const PortfolioInputForm: React.FC = () => {
 
     // 입력된 검색어가 있으면 필터링된 기술 스택을 보여주고, 없으면 검색 결과를 비움
     if (query) {
-      const results = skills.filter(skill =>
-        skill.name.toLowerCase().includes(query.toLowerCase())
-      )
+      const results = skills.filter((skill) => skill.name.toLowerCase().includes(query.toLowerCase()))
       setSearchResults(results)
     } else {
       setSearchResults([])
@@ -55,52 +93,104 @@ const PortfolioInputForm: React.FC = () => {
   }
 
   const handleRemoveTechStack = (stackToRemove: number) => {
-    setSelectedTechStack(selectedTechStack.filter(stack => stack !== stackToRemove))
+    setSelectedTechStack(selectedTechStack.filter((stack) => stack !== stackToRemove))
   }
 
   const handleSkillClick = (skillId: number) => {
     const alreadySelected = selectedTechStack.includes(skillId)
     if (alreadySelected) {
-      setSelectedTechStack(selectedTechStack.filter(id => id !== skillId))
+      setSelectedTechStack(selectedTechStack.filter((id) => id !== skillId))
     } else {
       setSelectedTechStack([...selectedTechStack, skillId])
     }
   }
 
+  const renderSearchResults = () => {
+    if (!searchQuery) {
+      return null // 검색어가 없으면 아무 것도 렌더링하지 않음
+    }
+
+    if (searchResults.length > 0) {
+      return (
+        <List spacing={2} borderWidth="1px" borderRadius="md" p={4}>
+          {searchResults.map((skill) => (
+            <ListItem key={skill.id} onClick={() => handleTechStackSelect(skill.id)} cursor="pointer">
+              {skill.name}
+            </ListItem>
+          ))}
+        </List>
+      )
+    }
+
+    return (
+      <Box textAlign="center" p={4} borderWidth="1px" borderRadius="md">
+        검색 결과가 없습니다.
+      </Box>
+    )
+  }
+
   return (
     <Box px={8}>
       {/* 기본 설정 */}
-      <Text fontSize="xl" fontWeight="bold" mt={3}>기본 설정</Text>
+      <Text fontSize="xl" fontWeight="bold" mt={3}>
+        기본 설정
+      </Text>
       <Divider my={2} />
-      <InformationBox>
-        · 필수 입력사항입니다.
-      </InformationBox>
+      <InformationBox>· * 표시가 된 항목은 필수 입력 항목입니다.</InformationBox>
 
-      <InputTextbox formLabel="포트폴리오 이름" placeHolder="포트폴리오 이름을 입력하세요" value={portfolioName} onChange={setPortfolioName} />
-      <InputTextbox formLabel="상부 타이틀" placeHolder="상부 타이틀을 입력하세요" value={title} onChange={setTitle} />
-      <InputTextbox formLabel="메인 소개글" placeHolder="메인 소개글을 입력하세요" value={description} onChange={setDescription} />
-      <InputImage formLabel="대표 사진" image={image} alt="대표 사진 미리보기" onClick={setImage} buttonLabel="대표 사진 추가" />
-
+      <InputTextbox
+        formLabel="포트폴리오 이름*"
+        placeHolder="포트폴리오 이름을 입력하세요"
+        value={portfolioName}
+        onChange={setPortfolioName}
+      />
+      <InputTextbox formLabel="상부 타이틀*" placeHolder="상부 타이틀을 입력하세요" value={title} onChange={setTitle} />
+      <InputTextbox
+        formLabel="메인 소개글*"
+        placeHolder="메인 소개글을 입력하세요"
+        value={description}
+        onChange={setDescription}
+      />
+      <InputImage
+        formLabel="대표 사진*"
+        image={image}
+        alt="대표 사진 미리보기"
+        onImageChange={setImage}
+        buttonLabel="대표 사진 추가"
+      />
       {/* 임베드 링크 */}
-      <Text fontSize="xl" fontWeight="bold" mt={20}>임베드 링크</Text>
+      <Text fontSize="xl" fontWeight="bold" mt={20}>
+        임베드 링크
+      </Text>
       <Divider my={2} />
-      <InformationBox>
-        · 선택 입력사항입니다.
-      </InformationBox>
+      <InformationBox>· 선택 입력사항입니다.</InformationBox>
 
-      <InputTextbox formLabel="깃허브 링크" placeHolder="깃허브 링크를 입력하세요" value={githubLink} onChange={setGithubLink} />
-      <InputTextbox formLabel="블로그 링크" placeHolder="블로그 링크를 입력하세요" value={blogLink} onChange={setBlogLink} />
+      <InputTextbox
+        formLabel="깃허브 링크"
+        placeHolder="깃허브 링크를 입력하세요"
+        value={githubLink}
+        onChange={setGithubLink}
+      />
+      <InputTextbox
+        formLabel="블로그 링크"
+        placeHolder="블로그 링크를 입력하세요"
+        value={blogLink}
+        onChange={setBlogLink}
+      />
 
       {/* 기술 스택 */}
-      <Text fontSize="xl" fontWeight="bold" mt={20}>기술 스택</Text>
+      <Text fontSize="xl" fontWeight="bold" mt={20}>
+        기술 스택
+      </Text>
       <Divider mt={2} mb={4} />
       <InformationBox>
         · 이용자가 자주 사용하는 기술 스택입니다. 자신의 직무와 관련된 스킬을 추가해보세요.
+        <br />· 필수 입력 항목입니다.
       </InformationBox>
 
       {/* 기술 스택 popular */}
       <HStack spacing={4} mb={4}>
-        {popularSkills.map(skill => {
+        {popularSkills.map((skill) => {
           const isSelected = selectedTechStack.includes(skill.id)
           return (
             <Button
@@ -122,35 +212,13 @@ const PortfolioInputForm: React.FC = () => {
       {/* 기술 스택 검색 */}
       <FormControl mb={4}>
         <InputGroup>
-          <Input
-            placeholder="기술 스택을 검색해주세요"
-            value={searchQuery}
-            onChange={handleSearchInputChange}
-          />
+          <Input placeholder="기술 스택을 검색해주세요" value={searchQuery} onChange={handleSearchInputChange} />
           <InputRightElement>
             <Icon as={FaSearch} color="gray.400" /> {/* 장식용 아이콘 */}
           </InputRightElement>
         </InputGroup>
       </FormControl>
-      <Box mb={4}>
-        {searchQuery && searchResults.length > 0 ? (
-          <List spacing={2} borderWidth="1px" borderRadius="md" p={4}>
-            {searchResults.map(skill => (
-              <ListItem
-                key={skill.id}
-                onClick={() => handleTechStackSelect(skill.id)}
-                cursor="pointer"
-              >
-                {skill.name}
-              </ListItem>
-            ))}
-          </List>
-        ) : searchQuery ? (
-          <Box textAlign="center" p={4} borderWidth="1px" borderRadius="md">
-            검색 결과가 없습니다.
-          </Box>
-        ) : null }
-      </Box>
+      <Box mb={4}>{renderSearchResults()}</Box>
 
       {/* 선택된 항목 */}
       <FormControl mb={4}>
@@ -158,8 +226,8 @@ const PortfolioInputForm: React.FC = () => {
         <Box borderWidth="1px" borderRadius="md" p={4} textAlign={selectedTechStack.length === 0 ? 'center' : 'left'}>
           <Wrap>
             {selectedTechStack.length > 0 ? (
-              selectedTechStack.map(stackId => {
-                const skill = skills.find(skill => skill.id === stackId)
+              selectedTechStack.map((stackId) => {
+                const skill = skills.find((s) => s.id === stackId)
                 return (
                   <WrapItem key={stackId} mr={2}>
                     <Tag
@@ -173,7 +241,7 @@ const PortfolioInputForm: React.FC = () => {
                       fontWeight="normal"
                     >
                       <TagLabel>{skill?.name}</TagLabel>
-                      <TagCloseButton onClick={() => handleRemoveTechStack(stackId)} color="brand.primary1"/>
+                      <TagCloseButton onClick={() => handleRemoveTechStack(stackId)} color="brand.primary1" />
                     </Tag>
                   </WrapItem>
                 )
---
+++ src/components/templates/PortfolioFormPage.tsx
@@ -0,0 +1,274 @@
+'use client'
+
+import React, { useEffect, useState } from 'react'
+import { Box, Divider } from '@chakra-ui/react'
+import { Project } from '@/types/data'
+import { useRouter } from 'next/navigation'
+import DashboardNavBar from '../organisms/DashboardNavBar'
+import { Heading } from '../atoms/Text'
+import PortfolioInputForm from '../organisms/PortfolioInputForm'
+import PortfolioInputFooter from '../molecules/PortfolioInputFooter'
+import ProjectInputForm from '../organisms/ProjectInputForm'
+
+const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL
+
+interface PortfolioFormPageProps {
+  id?: number
+}
+
+const PortfolioFormPage: React.FC<PortfolioFormPageProps> = ({ id }) => {
+  const isEditMode = !!id
+
+  const [page, setPage] = useState(0)
+  const nextPage = () => {
+    setPage(1)
+  }
+  const prevPage = () => {
+    setPage(0)
+  }
+  const router = useRouter()
+
+  // First Page States
+  const [portfolioName, setPortfolioName] = useState('')
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [imageFile, setImageFile] = useState<File | string | null>(null) // 이미지 파일로 변경
+  const [githubLink, setGithubLink] = useState('')
+  const [blogLink, setBlogLink] = useState('')
+  const [selectedTechStack, setSelectedTechStack] = useState<number[]>([])
+
+  // Project Page States
+  const [projects, setProjects] = useState<Project[]>([])
+
+  const checkRequiredFields = () => {
+    const missingFields = []
+
+    // Portfolio 필수 필드 체크
+    if (!portfolioName) missingFields.push('포트폴리오 이름')
+    if (!title) missingFields.push('제목')
+    if (!description) missingFields.push('설명')
+
+    // Project 필수 필드 체크
+    projects.forEach((project, index) => {
+      if (!project.name) missingFields.push(`프로젝트 ${index + 1}의 이름`)
+      if (!project.description) missingFields.push(`프로젝트 ${index + 1}의 설명`)
+      if (!project.startDate) missingFields.push(`프로젝트 ${index + 1}의 시작 날짜`)
+      if (!project.endDate) missingFields.push(`프로젝트 ${index + 1}의 종료 날짜`)
+    })
+
+    return missingFields
+  }
+
+  const getPortfolioData = async () => {
+    const token = localStorage.getItem('token')
+    if (!token) {
+      router.push('/')
+      throw new Error('Token not found')
+    }
+
+    const response = await fetch(`${apiUrl}/api/portfolio/${id}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    const res = await response.json()
+    if (response.status === 401) {
+      console.error('토큰이 만료되었습니다.')
+      localStorage.removeItem('token')
+      router.push('/')
+    }
+
+    if (response.ok) {
+      const portfolio = res.data
+      setPortfolioName(portfolio.file_name)
+      setTitle(portfolio.title)
+      setDescription(portfolio.description)
+      setGithubLink(portfolio.github_link)
+      setBlogLink(portfolio.blog_link)
+      setImageFile(`${apiUrl}/uploads/${portfolio.image}`)
+      const skillIds = portfolio.portfolioSkills.map((skill: any) => skill.skill_id)
+      setSelectedTechStack(skillIds)
+
+      // eslint-disable-next-line no-underscore-dangle
+      const fetchedProjects = portfolio.__projects__.map((project: any) => ({
+        id: project.id,
+        name: project.name,
+        description: project.description,
+        githubLink: project.github_link,
+        siteLink: project.site_link,
+        startDate: project.start_date ? project.start_date.split('-').slice(0, 2).join('-') : '', // 날짜 값이 없을 때 빈 문자열 반환
+        endDate: project.end_date ? project.end_date.split('-').slice(0, 2).join('-') : '', // 날짜 값이 없을 때 빈 문자열 반환
+        image: `${apiUrl}/uploads/${project.image}`, // 이미지는 서버에서 반환된 파일 경로
+        readmeFile: project.readme_file, // README 파일 경로
+        selectedTechStack: project.projectSkills ? project.projectSkills.map((skill: any) => skill.skill_id) : [],
+      }))
+
+      setProjects(fetchedProjects)
+      console.log(res.data)
+    } else {
+      console.error('포트폴리오 불러오기 실패:', res.message)
+    }
+  }
+
+  const updatePortfolio = async (token: any, formData: any) => {
+    const response = await fetch(`${apiUrl}/api/portfolio/${id}`, {
+      method: 'PATCH',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: formData,
+    })
+
+    const res = await response.json()
+    if (response.status === 401) {
+      console.error('토큰이 만료되었습니다.')
+      localStorage.removeItem('token')
+      router.push('/')
+    }
+
+    if (response.ok) {
+      console.log('포트폴리오 저장 성공:', res)
+      router.push('/dashboard')
+    } else {
+      console.error('포트폴리오 저장 실패:', res.message)
+    }
+  }
+
+  const createPortfolio = async (token: any, formData: any) => {
+    const response = await fetch(`${apiUrl}/api/portfolio`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: formData,
+    })
+
+    const res = await response.json()
+    if (response.status === 401) {
+      console.error('토큰이 만료되었습니다.')
+      localStorage.removeItem('token')
+      router.push('/')
+    }
+
+    if (response.ok) {
+      console.log('포트폴리오 저장 성공:', res)
+      router.push('/dashboard')
+    } else {
+      console.error('포트폴리오 저장 실패:', res.message)
+    }
+  }
+
+  const handleSubmit = async () => {
+    const missingFields = checkRequiredFields()
+
+    if (missingFields.length > 0) {
+      alert(`다음 필드를 작성해야 합니다: \n${missingFields.join(', ')}`)
+      return
+    }
+
+    try {
+      const token = localStorage.getItem('token')
+      if (!token) {
+        router.push('/')
+        throw new Error('Token not found')
+      }
+
+      const formData = new FormData()
+      formData.append('file_name', portfolioName)
+      formData.append('title', title)
+      formData.append('description', description)
+
+      // 이미지 파일이 있을 경우 추가
+      if (imageFile) {
+        formData.append('image', imageFile) // "image"는 서버에서 처리할 필드 이름
+      }
+
+      formData.append('github_link', githubLink)
+      formData.append('blog_link', blogLink)
+      formData.append('skills', JSON.stringify(selectedTechStack))
+      console.log(JSON.stringify(selectedTechStack))
+
+      // 프로젝트 정보와 파일 처리
+      projects.forEach((project, index) => {
+        formData.append(`projects[${index}][id]`, project.id.toString())
+        formData.append(`projects[${index}][name]`, project.name)
+        formData.append(`projects[${index}][description]`, project.description)
+        formData.append(`projects[${index}][github_link]`, project.githubLink)
+        formData.append(`projects[${index}][site_link]`, project.siteLink)
+        formData.append(`projects[${index}][start_date]`, `${project.startDate}-01`)
+        formData.append(`projects[${index}][end_date]`, `${project.endDate}-01`)
+        formData.append(`projects[${index}][skills]`, JSON.stringify(project.selectedTechStack))
+
+        // 프로젝트 이미지 파일이 있을 경우 추가
+        if (project.image instanceof File) {
+          formData.append(`projects[${index}][image]`, project.image) // 개별 프로젝트 이미지
+        }
+
+        // 프로젝트 README 파일이 있을 경우 추가
+        if (project.readmeFile) {
+          formData.append(`projects[${index}][readme_file]`, project.readmeFile)
+        }
+      })
+
+      if (isEditMode) {
+        updatePortfolio(token, formData)
+      } else {
+        createPortfolio(token, formData)
+      }
+    } catch (error) {
+      console.error('에러 발생:', error)
+    }
+  }
+
+  useEffect(() => {
+    if (isEditMode) {
+      getPortfolioData()
+    }
+  }, [])
+
+  return (
+    <div>
+      <DashboardNavBar />
+      <hr />
+      <Box mx="10" mt="5">
+        {isEditMode ? (
+          <Heading content="포트폴리오 수정" fontSize="2xl" color="brand.text1" />
+        ) : (
+          <Heading content="포트폴리오 생성" fontSize="2xl" color="brand.text1" />
+        )}
+        {page === 0 ? (
+          <Box>
+            <PortfolioInputForm
+              portfolioName={portfolioName}
+              setPortfolioName={setPortfolioName}
+              title={title}
+              setTitle={setTitle}
+              description={description}
+              setDescription={setDescription}
+              image={imageFile}
+              setImage={setImageFile} // 이미지 파일로 변경
+              githubLink={githubLink}
+              setGithubLink={setGithubLink}
+              blogLink={blogLink}
+              setBlogLink={setBlogLink}
+              selectedTechStack={selectedTechStack}
+              setSelectedTechStack={setSelectedTechStack}
+            />
+            <Divider />
+            <PortfolioInputFooter showNextButton onNextClick={nextPage} />
+          </Box>
+        ) : (
+          <Box>
+            <ProjectInputForm projects={projects} setProjects={setProjects} />
+            <Divider />
+            <PortfolioInputFooter showPrevButton onPrevClick={prevPage} showSaveButton onSaveClick={handleSubmit} />
+          </Box>
+        )}
+      </Box>
+    </div>
+  )
+}
+
+export default PortfolioFormPage
